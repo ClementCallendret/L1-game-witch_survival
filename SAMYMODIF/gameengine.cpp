d4c56bc837ca0dcf6fa67dc34bad6530cb03e2d4
Samsamy534 <99737503+Samsamy534@users.noreply.github.com>
2022-02-24 16:35:09 +0100

    lamap
    houseeeeeeee

---
+++ SAMYMODIF/gameengine.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+#include "gameengine.hpp"
+#include "gamestate.hpp"
+#include "game/Player.h"
+
+void CGameEngine::Init(const char* title, float width, float height, int bpp, bool fullscreen)
+{
+
+	// initialize SDL
+	sf::VideoMode resolution(width, height);
+	screen = new sf::RenderWindow(resolution, title);
+	screen->setFramerateLimit(60);
+
+	m_fullscreen = fullscreen;
+	m_running = true;
+
+	std::cout << "CGameEngine Init" << std::endl ;
+}
+
+void CGameEngine::Cleanup()
+{
+	// cleanup the all states
+	while ( !states.empty() ) {
+		states.top()->Cleanup();
+		states.pop();
+	}
+	//delete screen; <-- a tester si vrmt necessaire ou pas
+	//delete view; <-- a tester aussi 
+	// switch back to windowed mode so other 
+	// programs won't get accidentally resized
+	if ( m_fullscreen ) {
+		screen->setSize(sf::Vector2u(1200, 800));
+	}
+
+	screen->close();
+	printf("CGameEngine Cleanup\n");
+}
+
+void CGameEngine::ChangeState(CGameState* state) 
+{
+	// cleanup the current state
+	if ( !states.empty() ) {
+		states.top()->Cleanup();
+		states.pop();
+	}
+
+	// store and init the new state
+	states.push(state);
+	states.top()->Init();
+	printf("CGameEngine ChangeState done\n");
+}
+
+void CGameEngine::PushState(CGameState* state)
+{
+	// pause current state
+	if ( !states.empty() ) {
+		states.top()->Pause();
+	}
+
+	// store and init the new state
+	states.push(state);
+	states.top()->Init();
+}
+
+void CGameEngine::PopState()
+{
+	// cleanup the current state
+	if ( !states.empty() ) {
+		states.top()->Cleanup();
+		states.pop();
+	}
+
+	// resume previous state
+	if ( !states.empty() ) {
+		states.top()->Resume();
+	}
+}
+
+
+void CGameEngine::HandleEvents() 
+{
+	// let the state handle events
+	states.top()->HandleEvents(this);
+}
+
+void CGameEngine::Update() 
+{
+	// let the state update the game
+	states.top()->Update(this);
+}
+
+void CGameEngine::Draw() 
+{
+	// let the state draw the screen
+	states.top()->Draw(this);
+}
+
