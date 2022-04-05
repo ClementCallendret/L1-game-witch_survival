d39062f4d1e3f1f55d8addda022fdbe0555456dc
ClementCallendret <callendret.clement@gmail.com>
2022-04-05 11:58:43 +0200

    Optimizatiooooon

--- states/game/ennemi_arbre.cpp
+++ states/game/ennemi_arbre.cpp
@@ -1,18 +1,18 @@
 #include "ennemi_arbre.h"
 #include <cmath>
 
-arbre::arbre(Player* j, sf::Vector2f loc): Ennemi(j, loc){
-    speed = 0.7;
-    PV = 40;
-    degat = 2;
-    xp = 2;
-    name = "Arbre";
-    ratio = 0.2;
-    if (!texture.loadFromFile("media/arbre.png"))
+arbre::arbre(Player* j, sf::Vector2f loc): Ennemi(j, loc){ //paramètre utilisé pour les deplacements de l'ennemi car il suit le joueur
+    speed = 0.7; //vitesse
+    PV = 40; //point de vie
+    degat = 2; //degats
+    xp = 2; //experience donnee quand tué
+    name = "Arbre"; //nom
+    ratio = 0.2; //Pour redimensionner l'image en jeu : image en jeu = image de base * ratio
+    if (!texture.loadFromFile("media/arbre.png")) //texture de l'arbre
     return;
-    texture.loadFromFile("media/arbre.png");
-    sprite.setTexture(texture);
-    anim = Animation(sprite, 6,sf::Vector2i(145,140),290,280,0.2,0.2);
+    texture.loadFromFile("media/arbre.png"); 
+    sprite.setTexture(texture); //application de la texture
+    anim = Animation(sprite, 6,sf::Vector2i(145,140),290,280,0.2,0.2); //Animation de l'ennemi
     //nom, nb image, milieu, LARGEUR, HAUTEUR, ratio, vitesse
     taille = sf::Vector2f(30,30); //pour la hitbox
 
